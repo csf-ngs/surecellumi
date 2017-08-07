a7cd88c7327f1f7b60d154f0dd2a57c4f09bb3e2
Ido Tamir <ido.tamir@vbcf.ac.at>
2017-08-07 09:43:13 +0200

    pytest starting

--- tests/umi.py
+++ tests/umi.py
@@ -1,100 +1,11 @@
-import unittest
+import pytest
 
 import distance
 from cutadapt.align import Aligner
 from cutadapt.adapters import ANYWHERE
 
-PS=5
-PE=48
-L1="TAGCCATCGCATTGC" 
-L2="TACCTCTGAGCTGAA" 
-LINKER = L1+"NNNNNN"+L2 
 
-def loadBarcodes():
-    barcodes = []
-    with open("barcodes.txt") as b:
-         for l in b:
-             barcodes.append(l.strip())    
-    return barcodes 
-
-BARCODES = loadBarcodes()
-BARCODESMAP = dict(zip(BARCODES,BARCODES))
-STATSCOUNTER = { "noLL": 0, "noL1orL2": 0, "L1not6fromL2": 0, "noACGorGAC": 0, "extracted": 0 }
-
-##mutation rate full linker: length is 6 + 15+6+15 + 6 = 48: 0.05
-##mutation rate linker part: 0.1 
-def findLinker(readPart, linker, mut):
-    aligner = Aligner(readPart, mut, ANYWHERE, wildcard_ref=True,wildcard_query=True)
-    l = aligner.locate(linker)
-    if l is None:
-       return None       
-    rStart = l[0]
-    rEnd = l[1]
-    matches = l[4]
-    muts = l[5]
-    linkerLen = len(linker)
-    mLinkerLen = l[3] - l[2]
-    mReadLen = rEnd - rStart
-    if matches + muts >= linkerLen: #mutations, deletions
-       return l
-  
-##returns None or (ls,le) => for stats
-##tuple: (l1s,l1e,l2s,l2e) 
-##check l1e + 6 == l2s
-def positionLinker(readPart):
-    full = findLinker(readPart, LINKER, 0.05)
-    if full is not None:
-       l1 = findLinker(readPart, L1, 0.1)
-       l2 = findLinker(readPart, L2, 0.1)
-       if l1 is not None and l2 is not None:
-          if l1[1] + 6 == l2[0]:
-             return (l1[0],l1[1],l2[0],l2[1])
-          else:
-             STATSCOUNTER["L1not6fromL2"] += 1
-       else:
-          STATSCOUNTER["noL1orL2"] += 1
-    else:
-       STATSCOUNTER["noLL"] += 1
-
-def getUMI(read, linker2End):
-    acg = read[(linker2End+6):(linker2End+9)]
-    gac = read[(linker2End+17):(linker2End+20)]
-    if acg == "ACG" and gac == "GAC":
-       umi = read[(linker2End+9):(linker2End+17)]    
-       return umi
-    else:
-       STATSCOUNTER["noACGorGAC"] += 1
-
-def getBCs(read, linkers):
-    linker1Start = linkers[0] + PS
-    linker1End = linkers[1] + PS
-    linker2Start = linkers[2] + PS
-    linker2End = linkers[3] + PS
-    umi = getUMI(read, linker2End) 
-    if umi is not None:
-       bc1 = read[(linker1Start-6):linker1Start]
-       bc2 = read[linker1End:linker2Start]
-       bc3 = read[linker2End:(linker2End+6)]
-       return (bc1, bc2, bc3, umi)
-
-def correctBC(bc):
-    if BARCODESMAP.get(bc) is not None:
-       return bc
-    distances = filter(lambda m: distance.levenshtein(bc, m) <=  1, BARCODES)      
-    if len(distances) == 1:
-       return distances[0]
-
-def extractBCsUMI(read):
-    readPart = read[PS:PE]
-    linkers = positionLinker(readPart)
-    if linkers is not None and len(linkers) == 4:
-       bcsU = getBCs(read, linkers) 
-       bc1, bc2, bc3, umi = correctBC(bcsU[0]), correctBC(bcsU[1]), correctBC(bcsU[2]), bcsU[3] 
-       STATSCOUNTER["extracted"] += 1
-       return (bc1, bc2, bc3, umi)        
-
-class TestLinkerFinder(unittest.TestCase):
-     
+    
      def test_findLinker(self):
 	 r1 = "TAGCCATCGCATTGCAGCAGCTACCTCTGAGCTGAA"
 	 #"GGGGGG"+"TAGCCATCGCATTGCAGCAGCTACCTCTGAGCTGAA"+"GGGGGG"
@@ -146,5 +57,3 @@ class TestLinkerFinder(unittest.TestCase):
 
 
 
-if __name__ == '__main__':
-     unittest.main()
