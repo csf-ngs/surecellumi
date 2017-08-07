968e75b02f03ce378b1ea3f12c7f2115e5739322
Ido Tamir <ido.tamir@vbcf.ac.at>
2017-08-07 09:43:30 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='surecellumi',
+      version='0.1',
+      description='SureCell WTA UMI extraction',
+      url='http://github.com/csf/surecellumi',
+      author='Ido M. Tamir',
+      author_email='ido.tamir@vbcf.ac.at',
+      license='GPLv3',
+      packages=['surecellumi'],
+      install_requires=[
+          'cutadapt',
+          'Distance'
+      ],
+      setup_requires=['pytest-runner'],
+      tests_requires=['pytest'],
+      include_package_data=True,
+      scripts=['bin/mergeUmi'],
+      zip_safe=False)
